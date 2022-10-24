a29313a6da02e079a7d88c43338a02607430210e
scottemac00 <scott.e.mccraney@gmail.com>
2022-10-23 21:13:01 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -8,31 +8,23 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 #Initialize variable to count total votes and make it zero
 total_votes = 0
-
 # Declare a list to get candidate names from the csv
 candidate_options = []
-
 # Create a dictionary with key = candidate and value = vote count
 candidate_votes = {}
-
 # Decleare a variable that holds an empty string value for the winning candidate
 winning_candidate = ""
-
 # Declare a variable for the winning count and make it zero
 winning_count = 0
-
 #Declare a variable for the winning percentage and make it zero
 winning_percentage = 0
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
-
     # Read the header row
     headers = next(file_reader)
-    
     # Print each row in the CSV file
     for row in file_reader:
         # Add to the total vote count
@@ -48,19 +40,36 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
+# Save the results to text file
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+# Save the final vote count to the text file
+    txt_file.write(election_results)
     # Iterate through the candidate list
     for candidate_name in candidate_votes:
-        # Retrieve vote count of candidate
+                    # Retrieve vote count of candidate
         votes = candidate_votes[candidate_name]
-        # Calculate the percentage of votes
+                    # Calculate the percentage of votes
         vote_percentage = float(votes) / float(total_votes) * 100
-        # Print the candidate name and percentage of votes
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n") 
+                    # Print the candidate name and percentage of votes
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+         # Print each candidate, their voter count, and their % to the terminal
+        print(candidate_results)
+        # Save the candidate results to text file
+        txt_file.write(candidate_results) 
         # Determine winning vote count and candidate
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
+            winning_candidate = candidate_name
             winning_percentage = vote_percentage  
-            winning_candidate = candidate_name 
+        # Print the winning candidates' results         
     winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
@@ -68,29 +77,22 @@ with open(file_to_load) as election_data:
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-------------------------\n")
     print (winning_candidate_summary)
+        # Save the winning candidate's results to the text file
+    txt_file.write(winning_candidate_summary)
 
 
 
-       
-  
-
+        
     
 
-
-    # The data we need to retrieve.
-    # 1. Get the list of names of the candidates
-    # 2. Add a vote count for each candidate
-    # 3. Get the total votes for each candidate
-    # 4. Get the total votes cast for the election
-    # 5. Calculate the percentage of votes won by each candidate
-    # 6. Calculate the winner of the election based on popular vote
-    #print the file object
-   
+        
 
 
-
-
-
-
-
-
+        # The data we need to retrieve.
+        # 1. Get the list of names of the candidates
+        # 2. Add a vote count for each candidate
+        # 3. Get the total votes for each candidate
+        # 4. Get the total votes cast for the election
+        # 5. Calculate the percentage of votes won by each candidate
+        # 6. Calculate the winner of the election based on popular vote
+        #print the file object
